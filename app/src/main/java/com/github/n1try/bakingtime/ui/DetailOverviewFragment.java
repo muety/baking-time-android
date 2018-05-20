b797da8dcb49a07c27c6954ee4dfe1dcbd8d7309
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2018-05-20 15:09:41 +0200

    Show steps.

--- app/src/main/java/com/github/n1try/bakingtime/ui/DetailOverviewFragment.java
+++ app/src/main/java/com/github/n1try/bakingtime/ui/DetailOverviewFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import com.github.n1try.bakingtime.R;
 import com.github.n1try.bakingtime.model.Recipe;
 import com.github.n1try.bakingtime.model.RecipeIngredient;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +29,7 @@ public class DetailOverviewFragment extends Fragment {
     GridView stepsList;
 
     private Recipe mRecipe;
+    private ArrayAdapter<String> mStepsAdapter;
 
     public static DetailOverviewFragment newInstance(Recipe recipe) {
         DetailOverviewFragment fragment = new DetailOverviewFragment();
@@ -37,6 +43,14 @@ public class DetailOverviewFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mRecipe = getArguments().getParcelable(MainActivity.KEY_RECIPE_ID);
+        mStepsAdapter = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_list_item_1,
+                IntStream.range(0, mRecipe.getSteps().size())
+                        .mapToObj(i -> Pair.create(i, mRecipe.getSteps().get(i)))
+                        .map(p -> String.format("%s. %s", p.first, p.second.getShortDescription()))
+                        .collect(Collectors.toList())
+        );
     }
 
     @Nullable
@@ -46,6 +60,7 @@ public class DetailOverviewFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         ingredientsText.setText(TextUtils.concat((CharSequence[]) mRecipe.getIngredients().stream().map(RecipeIngredient::format).toArray(i -> new CharSequence[i])));
+        stepsList.setAdapter(mStepsAdapter);
 
         return view;
     }
